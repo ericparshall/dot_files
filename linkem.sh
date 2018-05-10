b91e5e0292fd9e84d7cfaf9d7e7b114e7a70d9df
Eric Parshall <eparshall@nav.com>
2018-05-10 13:49:12 -0600

    Latest changes

--- linkem.sh
+++ linkem.sh
@@ -3,6 +3,7 @@ ln -s $HOME/dot_files/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dot_files/.gitignore_global $HOME/.gitignore_global
 ln -s $HOME/dot_files/.vimrc $HOME/.vimrc
 ln -s $HOME/dot_files/.screenrc $HOME/.screenrc
+ln -s $HOME/dot_files/.hyper.js $HOME/.hyper.js
 sed -i -e '/source '"${HOME//\//\\/}"'\/dot\_files\/.bash_profile/d' $HOME/.bash_profile
 echo "source $HOME/dot_files/.bash_profile" >> $HOME/.bash_profile
 
